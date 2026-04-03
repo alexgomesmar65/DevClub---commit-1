8b737c8d74a0db62883a3ecf0673ab0a77b6b267
alexgomesmar65 <alexandre.gomestec@gmail.com>
2026-04-03 14:38:02 -0300

    atualização github

---
+++ Node/server.js
@@ -0,0 +1,18 @@
+// //ANTIGO
+// const express = require('express');
+
+// NOVO
+
+import express from "express";
+
+const app = express();
+
+app.get("/usuarios", (req, res) => {
+  res.send("olá, rota acessada com sucesso!");
+});
+
+app.post("/usuarios", (req, res) => {
+  res.send("usuario criado com sucesso!");
+});
+
+app.listen(3000);
